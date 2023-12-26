a88b117847524d084c6d3db2e994cca6ebcacbe2
Pavel Karpenko <pavelkv@gmail.com>
2023-12-26 20:30:31 +0300

    feat: implement canQueenCaptureKing task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -72,8 +72,15 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  const diffX = Math.abs(king.x - queen.x);
+  const diffY = Math.abs(king.y - queen.y);
+
+  if (diffX === 0 || diffY === 0 || diffX === diffY) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
