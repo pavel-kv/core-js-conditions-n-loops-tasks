c14b147561628103222a3af1d131dd309871b2a8
Pavel Karpenko <pavelkv@gmail.com>
2023-12-26 20:35:00 +0300

    feat: implement isIsoscelesTriangle task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -101,8 +101,24 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === b && a === c && a !== 0) {
+    return true;
+  }
+
+  if (a === b && a + b > c && c !== 0) {
+    return true;
+  }
+
+  if (b === c && b + c > a && a !== 0) {
+    return true;
+  }
+
+  if (c === a && c + a > b && b !== 0) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
